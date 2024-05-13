ca17daa22e5ec965f81ffd08092079bb4828aa1d
gitsevero <thierrymuller2015@gmail.com>
2024-05-12 22:29:31 -0300

    iniciando controller UserController

---
+++ Controller/UserController.go
@@ -0,0 +1,19 @@
+package Controller
+
+import "github.com/gin-gonic/gin"
+
+func CreateUser(context *gin.Context) {
+
+}
+func UpdateUser(context *gin.Context) {
+
+}
+func GetUsers(context *gin.Context) {
+
+}
+func GetUser(context *gin.Context) {
+
+}
+func DeleteUser(context *gin.Context) {
+
+}
